b8ac96393d1be89e0b431d804959f497bff9a03d
ytiK0 <tiprson@gmail.com>
2022-09-09 18:48:24 +0300

    Add project files.

---
+++ sp API/forms.cs
@@ -0,0 +1,51 @@
+﻿//В этом пространстве имён собранны классы для стерилизации и дестерилизации JSON файлов использующий класс SPapi
+namespace forms
+{
+    //форма для стерелизации JSON для запроса сслки для оплаты
+    internal class PaymentForm
+    {
+        public int amount { get; private set; }
+        public string redirectUrl { get; private set; }
+        public string webhookUrl { get; private set; }
+        public string data { get; private set; }
+
+        public PaymentForm(int amount, string redirectUrl, string webhookUrl, string data)
+        {
+            this.amount=amount;
+            this.redirectUrl=redirectUrl;
+            this.webhookUrl=webhookUrl;
+            this.data=data;
+        }
+    }
+    //форма для стерелизации JSON для совершения переводов
+    internal class PayForm
+    {
+        public string receiver { get; set; }
+        public int amount { get; set; }
+        public string comment { get; set; }
+
+        public PayForm(string receiver, int amount, string data)
+        {
+            this.receiver=receiver;
+            this.amount=amount;
+            this.comment=data;
+        }
+    }
+
+    //формы для дестерелизации ответов сервера
+    internal class PaymentUrlAnswer
+    {
+        public string url { get; set; }
+        public PaymentUrlAnswer(string url) { this.url=url; }
+    }
+    internal class BalanceAnswer
+    {
+        public int balance { get; set; }
+        public BalanceAnswer(int balance) { this.balance=balance; }
+    }
+    internal class UsernmeAnswer
+    {
+        public string username { get; set; }
+        public UsernmeAnswer(string username) { this.username=username; }
+    }
+}
---
+++ sp API/spAPI.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using forms;
+
+namespace SP_api
+{
+    public class SPapi
+    {
+        private string baseURL = ("https://spworlds.ru/api/public/");
+        private string key;
+        public HttpClient client;
+
+        /// <summary>
+        /// Создать экземпляр класса SPapi
+        /// </summary>
+        /// <param name="id">id вашей карты</param>
+        /// <param name="token">Токен вашей карты</param>
+        public SPapi(string id, string token)
+        {
+            key = Base64Encode(id + ":" + token);
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", key);
+        }
+        /// <summary>
+        /// Получение ссылки на оплату
+        /// </summary>
+        /// <param name="amount">Стоимость покупки</param>
+        /// <param name="redirectUrl">URL на которуюю попадёт пользователь после оплаты(по умолчанию spworlds.ru)</param>
+        /// <param name="webhookUrl">URL куда сервер направит запрос, чтобы оповестить ваш сервер об успешной оплате</param>
+        /// <param name="data">Строка до 100 символов, сюда можно поместить любые полезные данных</param>
+        /// <returns>Ссылка на оплату</returns>
+        public async Task<string> GetPaymentLink(int amount, string webhookUrl, string data, string redirectUrl = "https://spworlds.ru")
+        {
+            if (data.Length > 100) return "Cтрока data содержит больше 100 символов";
+            PaymentForm payForm = new PaymentForm(amount, redirectUrl, webhookUrl, data);
+            var paymentRequest = new StringContent(JsonConvert.SerializeObject(payForm), Encoding.UTF8, "application/json");
+            var jsonAnswer = await client.PostAsync("https://spworlds.ru/api/public/payment", paymentRequest).Result.Content.ReadAsStringAsync();
+            var url = JsonConvert.DeserializeObject<PaymentUrlAnswer>(jsonAnswer).url;
+            return url == null ? "Ошибка! Возможно ошибка в токене или id карты." : url;
+        }
+        /// <summary>
+        /// Получение баланса карты
+        /// </summary>
+        /// <returns>Числовое значение баланса карты</returns>
+        public async Task<int> GetBalance()
+        {
+            int balance = -1;
+            var jsonAnswer = await client.GetAsync(baseURL + "card").Result.Content.ReadAsStringAsync();
+            balance = JsonConvert.DeserializeObject<BalanceAnswer>(jsonAnswer).balance;
+            return balance;
+        }
+        /// <summary>
+        /// Получение имени пользователя игрока
+        /// </summary>
+        /// <param name="discordId">id игрока Discord</param>
+        /// <returns>Имя пользователя игрока</returns>
+        public async Task<string> GetUsername(string discordId)
+        {
+            var jsonAnswer = client.GetAsync(baseURL + "users/" + discordId).Result;
+            var username = JsonConvert.DeserializeObject<UsernmeAnswer>(await jsonAnswer.Content.ReadAsStringAsync()).username;
+            return username == null ? "Пользователь не найден." : username;
+        }
+        /// <summary>
+        /// Совершить перевод
+        /// </summary>
+        /// <param name="receiver">Номер карты получателя</param>
+        /// <param name="amount">Количество АР</param>
+        /// <param name="comment">Любой коментарий к переводу</param>
+        /// <returns>Статус перевода</returns>
+        public async Task<string> Pay(string receiver, int amount, string comment)
+        {
+            if (amount <= 0) return "Количество переводимых АРов не может быть равно нулю либо меньше нуля.";
+            HttpContent paymentRequest = new StringContent(JsonConvert.SerializeObject(new PayForm(receiver, amount, comment)), Encoding.UTF8, "application/json");
+            var status = await client.PostAsync(baseURL + "transactions", paymentRequest);
+            return status.StatusCode.ToString() == "OK" ? "Успешно!" : "Ошибка! Возможно такого номера карты нет.";
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
