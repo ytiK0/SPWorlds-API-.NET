2bc8c32f7ee0f8759e30919477688ba41723d524
ytiK0 <tiprson@gmail.com>
2022-10-22 11:34:43 +0300

    methods have been updated

--- sp API/spAPI.cs
+++ sp API/spAPI.cs
@@ -31,12 +31,12 @@ namespace SP_api
         /// <param name="webhookUrl">URL куда сервер направит запрос, чтобы оповестить ваш сервер об успешной оплате</param>
         /// <param name="data">Строка до 100 символов, сюда можно поместить любые полезные данных</param>
         /// <returns>Ссылка на оплату</returns>
-        public async Task<string> GetPaymentLink(int amount, string webhookUrl, string data, string redirectUrl = "https://spworlds.ru")
+        public string GetPaymentLink(int amount, string webhookUrl, string data, string redirectUrl = "https://spworlds.ru")
         {
             if (data.Length > 100) return "Cтрока data содержит больше 100 символов";
             PaymentForm payForm = new PaymentForm(amount, redirectUrl, webhookUrl, data);
             var paymentRequest = new StringContent(JsonConvert.SerializeObject(payForm), Encoding.UTF8, "application/json");
-            var jsonAnswer = await client.PostAsync("https://spworlds.ru/api/public/payment", paymentRequest).Result.Content.ReadAsStringAsync();
+            var jsonAnswer = client.PostAsync("https://spworlds.ru/api/public/payment", paymentRequest).Result.Content.ReadAsStringAsync().Result;
             var url = JsonConvert.DeserializeObject<PaymentUrlAnswer>(jsonAnswer).url;
             return url == null ? "Ошибка! Возможно ошибка в токене или id карты." : url;
         }
@@ -44,10 +44,10 @@ namespace SP_api
         /// Получение баланса карты
         /// </summary>
         /// <returns>Числовое значение баланса карты</returns>
-        public async Task<int> GetBalance()
+        public int GetBalance()
         {
             int balance = -1;
-            var jsonAnswer = await client.GetAsync(baseURL + "card").Result.Content.ReadAsStringAsync();
+            var jsonAnswer = client.GetAsync(baseURL + "card").Result.Content.ReadAsStringAsync().Result;
             balance = JsonConvert.DeserializeObject<BalanceAnswer>(jsonAnswer).balance;
             return balance;
         }
@@ -56,10 +56,10 @@ namespace SP_api
         /// </summary>
         /// <param name="discordId">id игрока Discord</param>
         /// <returns>Имя пользователя игрока</returns>
-        public async Task<string> GetUsername(string discordId)
+        public string GetUsername(string discordId)
         {
             var jsonAnswer = client.GetAsync(baseURL + "users/" + discordId).Result;
-            var username = JsonConvert.DeserializeObject<UsernmeAnswer>(await jsonAnswer.Content.ReadAsStringAsync()).username;
+            var username = JsonConvert.DeserializeObject<UsernmeAnswer>(jsonAnswer.Content.ReadAsStringAsync().Result).username;
             return username == null ? "Пользователь не найден." : username;
         }
         /// <summary>
@@ -69,12 +69,12 @@ namespace SP_api
         /// <param name="amount">Количество АР</param>
         /// <param name="comment">Любой коментарий к переводу</param>
         /// <returns>Статус перевода</returns>
-        public async Task<string> Pay(string receiver, int amount, string comment)
+        public string Pay(string receiver, int amount, string comment)
         {
             if (amount <= 0) return "Количество переводимых АРов не может быть равно нулю либо меньше нуля.";
             HttpContent paymentRequest = new StringContent(JsonConvert.SerializeObject(new PayForm(receiver, amount, comment)), Encoding.UTF8, "application/json");
-            var status = await client.PostAsync(baseURL + "transactions", paymentRequest);
-            return status.StatusCode.ToString() == "OK" ? "Успешно!" : "Ошибка! Возможно такого номера карты нет.";
+            var status = client.PostAsync(baseURL + "transactions", paymentRequest).Result.StatusCode;
+            return status != System.Net.HttpStatusCode.OK ? "Ошибка! Возможно такого номера карты нет." : "Успешно!";
         }
 
         private static string Base64Encode(string plainText)
